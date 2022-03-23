5627e8df1af6b8918a93a7601b51b4902b0d92cb
Aaron Garay <aaronroman93@hotmail.com>
2022-03-23 10:48:40 -0300

    mapeo de clases a tablas con JPA y creacion del controlador administrador

--- src/main/java/com/spring/ecommerce/entidades/Producto.java
+++ src/main/java/com/spring/ecommerce/entidades/Producto.java
@@ -1,17 +1,32 @@
 package com.spring.ecommerce.entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String descripcion;
     private String imagen;
     private Double precio;
     private Integer cantidad;
+    
+    @ManyToOne
+    private Usuario usuario;
 
     public Producto() {
     }
 
-    public Producto(String nombre, String descripcion, String imagen, Double precio, Integer cantidad) {
+    public Producto(Integer id, String nombre, String descripcion, String imagen, Double precio, Integer cantidad) {
+        this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.imagen = imagen;
@@ -65,6 +80,14 @@ public class Producto {
 
     public void setCantidad(Integer cantidad) {
         this.cantidad = cantidad;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 
     @Override
