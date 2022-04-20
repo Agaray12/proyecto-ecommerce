42a2b9537bc6f7e9c6799bef59c74635079f1868
Aaron Garay <aaronroman93@hotmail.com>
2022-04-20 02:35:14 -0300

    creacion de metodo POST para el guardado de un producto, llamado a dicho metodo y funcionalidad para mostrar productos para el administrador

--- src/main/java/com/spring/ecommerce/controladores/ProductoControlador.java
+++ src/main/java/com/spring/ecommerce/controladores/ProductoControlador.java
@@ -1,6 +1,10 @@
 package com.spring.ecommerce.controladores;
 
+import com.spring.ecommerce.entidades.Usuario;
+import com.spring.ecommerce.servicios.ProductoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +14,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/productos")
 public class ProductoControlador {
     
+    @Autowired
+    private ProductoServicio ps;
+    
     @GetMapping("")
-    public String show(){
+    public String show(ModelMap modelo){
+        modelo.addAttribute("productos", ps.listarProductos());
         return "productos/show";
     }
     
     @GetMapping("/create")
     public String create(){
         return "productos/create";
+    }
+    
+    @PostMapping("/create")
+    public String create(@RequestParam String nombre, @RequestParam String descripcion, @RequestParam Double precio, @RequestParam Integer cantidad){
+        Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+        ps.registrarProducto(nombre, descripcion, precio, cantidad, u);
+        return "redirect:/productos";
     }
 }
